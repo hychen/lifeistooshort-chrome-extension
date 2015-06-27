1ef663aa93181f853a9d66a6ba620dcdcfcb0ee1
陳信屹 <ossug.hychen@gmail.com>
2015-06-27 17:53:21 +0800

    use User-ID in ga.

--- app/js/libs/ga.js
+++ app/js/libs/ga.js
@@ -3,8 +3,32 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-window.onlyRunInProduction(function() {
-ga('create', 'UA-23039743-2', 'auto');
-ga('set', 'checkProtocolTask', function(){/* just do not throw sth. here */});
-ga('send', 'pageview', '/dashboard.html');
+function getRandomToken() {
+    // E.g. 8 * 32 = 256 bits token
+    var randomPool = new Uint8Array(32);
+    crypto.getRandomValues(randomPool);
+    var hex = '';
+    for (var i = 0; i < randomPool.length; ++i) {
+        hex += randomPool[i].toString(16);
+    }
+    // E.g. db18458e2782b2b77e36769c569e263a53885a9944dd0a861e5064eac16f1a
+    return hex;
+}
+
+chrome.storage.sync.get('userid', function(items) {
+    var userid = items.userid;
+    if (userid) {
+        useToken(userid);
+    } else {
+        userid = getRandomToken();
+        chrome.storage.sync.set({userid: userid}, function() {
+            useToken(userid);
+        });
+    }
+    function useToken(userid) {
+        // TODO: Use user id for authentication or whatever you want.
+        ga('create', 'UA-23039743-2', { 'userId': userid });
+        ga('set', 'checkProtocolTask', function(){/* just do not throw sth. here */});
+        ga('send', 'pageview', '/dashboard.html');
+    }
 });
